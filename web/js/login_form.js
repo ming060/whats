ff328c80cfa62580f43eea433625a760156938d1
ming060 <lym060@gmail.com>
2014-12-21 02:02:20 +0800

    login
    add login function to page

--- web/js/login_form.js
+++ web/js/login_form.js
@@ -1,24 +1,48 @@
 var LoginForm = React.createClass({
+  login: function(username, password) {
+    console.log('username: ' + username);
+    console.log('password: ' + password);
+    $.ajax({
+      type: "POST",
+      url: "http://192.168.1.69:3000/login",
+      data: {'username': username, 'password': password},
+      error(jqXHR, textStatus, errorThrown){
+        console.log('err');
+        alert("Login Fail");
+        console.log(jqXHR);
+      },
+      success(data, textStatus, jqXHR) {
+        console.log('data'+ data);
+      }
+    })
+    .done(function(msg) {
+      console.log('msg: '+ msg);
+      window.location.assign("show_messages.html");
+    });
+  },
 	handleSubmit: function(e) {
     e.preventDefault();
-    var username = this.names.username.getDOMNode().value.trim();
-    var password = this.names.password.getDOMNode().value.trim();
+    var username = this.refs.username.getDOMNode().value.trim();
+    var password = this.refs.password.getDOMNode().value.trim();
+
     if (!password || !username) {
+      alert("User Name & password should not be empty");
       return;
     }
     // TODO: send request to the server
-    this.names.username.getDOMNode().value = '';
-    this.names.password.getDOMNode().value = '';
+    this.login(username, password);
     return;
   },
   render: function() {
     return (
-      <form className="loginForm" method="post">
-        User Name: <input type="text" name="username" placeholder="User name" /><br/>
-        psassword: <input type="password" name="password" placeholder="Password" /><br/>
 
-         <input type="submit" value="login" />
+      <div className="center-div" >
+      <form className="loginForm" method="post" onSubmit={this.handleSubmit}>
+        User Name: <input type="text" ref="username" placeholder="User name" /><br/>
+        psassword: <input type="password" ref="password" placeholder="Password" /><br/>
+        <input type="submit" value="login" />
       </form>
+      </div>
     );
   }
 });
