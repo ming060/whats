c8e86324179e52043617225bd7a8e09387ff8551
smallblack9 <smallblack9@gmail.com>
2014-12-21 10:11:23 +0800

    show message with session data

--- web/js/show_list.js
+++ web/js/show_list.js
@@ -1,29 +1,73 @@
-var data = [
-  {fromwho: "Peter Hunt", content: "Hi, Im Peter", senttime: "2014/12/21"},
-  {fromwho: "Jordan Walke", content: "Hi, Im Jordan", senttime: "2014/12/21"}
-];
+
+var ShortMessage = React.createClass({
+	handleClick: function(){
+      popup('msgPopUpDiv');
+ 	},
+	render: function(){
+		return (
+			<div className="shortmessage"
+			onClick={this.handleClick}>
+				<h5 >{this.props.fromwho}</h5>
+				<div >{this.props.senttime}</div>				
+				<br />
+			</div>
+		);
+	}
+});
 
 var Message = React.createClass({
 	render: function(){
 		return (
-			<div className="message">
-				<h5>{this.props.fromwho}</h5>
-				<div>{this.props.children}</div>
-				<div>{this.props.senttime}</div>
+			<div className="message" >
+				<h5 >{this.props.fromwho}</h5>
+				<div >{this.props.children}</div>
+				<div >{this.props.senttime}</div>
+				<br />
 			</div>
 		);
 	}
 });
 
 var MessageList = React.createClass({
-	render: function() {
-		var messages = this.props.msglist.map(function(message){
+	loadMessagesFromServer: function() {
+		var username = Session.get('username');
+	    var password = Session.get('password');
+	    console.log(username);
+	    console.log(password);
+		$.ajax({
+			url: "http://192.168.1.69:3000/read",
+			type: 'POST',
+			data: {'username': username, 'password': password},
+			async: false,
+			success: function(data){
+				this.setState({msglist:data});
+				this.render();
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	getInitialState: function(){
+		return {msglist: []};
+	},
+	componentDidMount: function() {
+    	this.loadMessagesFromServer();
+	},
+
+  	render: function() {
+  		console.log("list: " + this.state.msglist);
+		var messages = this.state.msglist.map(function(message){
+			var date = new Date(message.Timestamp);
+			console.log("time: " + message.Timestamp);
 			return (
-				<Message fromwho={message.fromwho} senttime={message.senttime}>{message.content}</Message>
+
+				<ShortMessage fromwho={message.Provider} senttime={date.toLocaleString()} >
+				{message.Message}</ShortMessage>
 			);
 		});
 		return (
-			<div className="messageList">
+			<div className="messageList" id="msglist">
 				<h3>List</h3>
 				{messages}
 			</div>
@@ -32,6 +76,6 @@ var MessageList = React.createClass({
 });
 
 React.render(
-	<MessageList msglist={data}/>, 
+	<MessageList />, 
 	document.getElementById('content')
 );
